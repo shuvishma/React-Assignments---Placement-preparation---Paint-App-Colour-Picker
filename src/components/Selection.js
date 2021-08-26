2247f13bea6fbfaff565cae3101ac2750b9f7d57
shuvishma <36675563+shuvishma@users.noreply.github.com>
2021-08-26 17:30:13 +0530

    saved

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React from "react";
+import '../styles/Child.css';
 
+function Selection(props) {
+    let [bgColor, setBgColor] = React.useState({backgorund:''}) ;
+    const updateSelectionStyle = () => {
+        props.applyColor(setBgColor) ;
+    }
+    return (
+        <div className="fix-box" onClick={updateSelectionStyle} style={bgColor}>
+            <h2 className="subheading">Selection</h2>
+        </div>
+    )
+}
+
+export default Selection;
